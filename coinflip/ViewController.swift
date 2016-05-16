fcf9f5bcca390900673f7f312b1e2ad4072875ef
Neil Sarkar <neil.sarkar@gotinder.com>
2016-05-15 17:14:49 -0700

    Unstyled coin flip

--- coinflip/ViewController.swift
+++ coinflip/ViewController.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var resultLabel: UILabel!
 
+    @IBAction func buttonPressed(sender: AnyObject) {
+        let result = self.calculateResult();
+        self.showResult(result);
+    }
+ 
+    func calculateResult() -> Bool {
+        let flip = Int(arc4random_uniform(2))
+        return flip == 1;
+    }
+    
+    func showResult(result: Bool) {
+        if( result ) {
+            resultLabel.text = "Heads"
+        } else {
+            resultLabel.text = "Tails"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultLabel.text = ""
         // Do any additional setup after loading the view, typically from a nib.
     }
 
